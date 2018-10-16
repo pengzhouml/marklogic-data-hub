22cec2621b95ec023ae35e8ef3a00a543d075af3
Aries Yuwono <ayuwono@marklogic.com>
2018-10-16 10:41:30 -0700

    entity disclosure check with isDisplayed

--- quick-start/e2e/page-objects/flows/flows.ts
+++ quick-start/e2e/page-objects/flows/flows.ts
@@ -16,7 +16,7 @@ export class FlowPage extends AppPage {
   }
 
   isEntityCollapsed(entityName: string) {
-    return element(by.css(`div[data-entity="${entityName}"] .collapsed`)).isPresent();
+    return element(by.css(`div[data-entity="${entityName}"] .collapsed`)).isDisplayed();
   }
 
   clickEntityDisclosure(entityName: string) {
